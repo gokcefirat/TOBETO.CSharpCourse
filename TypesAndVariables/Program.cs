2d58134d5a67d7d0ac1f08ec29313a31e2c7d85e
Gokce <Gokce@THINKPAD>
2023-10-23 22:03:14 +0300

    Add project files.

---
+++ TypesAndVariables/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesAndVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Wold !!!");
+            Console.ReadKey();
+        }
+    }
+}
