5a0851c108c37ad293e1cacbe012479929817c03
gokcefirat <gokcealp89@gmail.com>
2023-10-23 23:23:46 +0300

    Yeni veri tipleri eklendi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -11,8 +11,38 @@ namespace TypesAndVariables
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Wold !!!");
-            Console.ReadKey();
+
+            //Value Types
+            //Console.WriteLine("Hello World!");
+
+            double number5 = 5.2;
+            decimal number6 = 10.6M;
+            char character = 'A';
+            bool condition = false;
+            byte number4 = 255;
+            short number3 = 32767;
+            int number1 = 215445648;
+            long number2 = -21542225552005455;
+            var number7 = 10;
+            number7 = 'A';
+            //number7 = "A";
+            Console.WriteLine("Number1 is {0}", number1);
+            Console.WriteLine("Number2 is {0}", number2);
+            Console.WriteLine("Number3 is {0}", number3);
+            Console.WriteLine("Number4 is {0}", number4);
+            Console.WriteLine("Character is {0}", (int)character);
+            Console.WriteLine("Number5 is {0}", number5);
+            Console.WriteLine("Number6 is {0}", number6);
+            Console.WriteLine((int)Days.Friday);
+            Console.ReadLine();
         }
+        enum Days
+        {     
+            Monday = 10, Tuesday = 20, Wednesday = 30,Thursday,Friday,Saturday,Sunday
+    
+        }    
+        
     }
+
+
 }
